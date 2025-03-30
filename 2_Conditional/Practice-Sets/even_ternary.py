b4e1d174122b20cb85e4586aeeeda4efb7dab848
Vivek Anand <va691187@gmail.com>
2025-03-30 17:56:27 +0530

    Second Commit

---
+++ 2_Conditional/Practice-Sets/even_ternary.py
@@ -0,0 +1,5 @@
+# Create a program to check if a given number is even or odd using the ternary operator.
+
+num = int(input("Enter Number : "))
+check = "Even!" if num%2==0 else "Odd!"
+print(check)
